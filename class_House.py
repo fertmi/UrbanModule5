27b7bccda97da222be84d80cab306e4a403fda91
fertmi <fertmi@mail.ru>
2024-12-26 01:22:24 +0300

    Задача "История строительства"

--- class_House.py
+++ class_House.py
@@ -1,4 +1,9 @@
 class House: #Создание класса дом
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
     def __init__(self, name, number_floor):
         self.name = name
         self.number_floor = number_floor
@@ -6,7 +11,7 @@ class House: #Создание класса дом
     def __len__(self): #Специальный метод определения длины дома - это количество этажей
         return self.number_floor
 
-    def end_floor(self, number):  # Метод для определения окончания слова "этаж"
+    def end_floor_rp(self, number):  # Метод для определения окончания слова "этаж" родительный падеж
         if number % 10 > 4 or number in range(10, 21) or number % 10 == 0:
             return 'этажей'
         elif number % 10 in range(2, 5):
@@ -14,46 +19,97 @@ class House: #Создание класса дом
         else:
             return 'этаж'
 
+    def end_floor_rp(self, number):  # Метод для определения окончания слова "этаж" 
+        if number % 10 > 4 or number in range(10, 21) or number % 10 == 0:
+            return 'этажей'
+        elif number % 10 in range(2, 5):
+            return 'этажа'
+        else:
+            return 'этаж'
+
+    def met_world_operations(self, other, operations): #Метод результата сравнения
+        operations_dict = {'=':' равно ','!=':' не равно ', '<':' меньше ', '<=':' меньше или равно ', '>':' больше ', '>=':' больше или равно ',}
+        if isinstance(other, House):
+            return f'{self.number_floor} {self.end_floor_rp(self.number_floor)} {self.name}'+operations_dict[operations]+f'{other.number_floor} {self.end_floor_rp(other.number_floor)} {other.name}.'
+        elif isinstance(other, int):
+            return f'{self.number_floor} {self.end_floor_rp(self.number_floor)} {self.name}' + operations_dict[operations] + f'{other} {self.end_floor_rp(other)}.'
+
     def __eq__(self, other):
         if isinstance(other, House):
-            return self.number_floor == other.number_floor
+            if self.number_floor == other.number_floor:
+                return self.met_world_operations(other,operations='=')
+            else:
+                return self.met_world_operations(other,operations='!=')
         elif isinstance(other, int):
-            return self.number_floor == other
+            if self.number_floor == other:
+                return self.met_world_operations(other, operations='=')
+            else:
+                return self.met_world_operations(other, operations='!=')
 
     def __lt__(self, other):
         if isinstance(other, House):
-            return self.number_floor < other.number_floor
+            if self.number_floor < other.number_floor:
+                return self.met_world_operations(other, operations='<')
+            else:
+                return False
         elif isinstance(other, int):
-            return self.number_floor < other
+            if self.number_floor < other:
+                return self.met_world_operations(other, operations='<')
+            else:
+                return False
 
     def __le__(self, other):
         if isinstance(other, House):
-            return self.number_floor <= other.number_floor
+            if self.number_floor <= other.number_floor:
+                return self.met_world_operations(other, operations='<=')
+            else:
+                return False
         elif isinstance(other, int):
-            return self.number_floor <= other
+            if self.number_floor <= other:
+                return self.met_world_operations(other, operations='<=')
+            else:
+                return False
 
     def __gt__(self, other):
         if isinstance(other, House):
-            return self.number_floor > other.number_floor
+            if self.number_floor > other.number_floor:
+                return self.met_world_operations(other, operations='>')
+            else:
+                return False
         elif isinstance(other, int):
-            return self.number_floor > other
+            if self.number_floor <= other:
+                return self.met_world_operations(other, operations='>')
+            else:
+                return False
 
     def __ge__(self, other):
         if isinstance(other, House):
-            return self.number_floor >= other.number_floor
+            if self.number_floor >= other.number_floor:
+                return self.met_world_operations(other, operations='>=')
+            else:
+                return False
         elif isinstance(other, int):
-            return self.number_floor >= other
+            if self.number_floor <= other:
+                return self.met_world_operations(other, operations='>=')
+            else:
+                return False
 
     def __ne__(self, other):
         if isinstance(other, House):
-            return self.number_floor != other.number_floor
+            if self.number_floor != other.number_floor:
+                return self.met_world_operations(other, operations='!=')
+            else:
+                return False
         elif isinstance(other, int):
-            return self.number_floor != other
+            if self.number_floor != other:
+                return self.met_world_operations(other, operations='!=')
+            else:
+                return False
 
     def __add__(self, value):
-        if isinstance(value, int):
-            self.number_floor += value
-            return self
+        #if isinstance(value, int):
+         self.number_floor += value
+         return self
 
     def __radd__(self, value):
         return self.__add__(value)
@@ -101,11 +157,15 @@ class House: #Создание класса дом
             return self
 
     def __str__(self): #Специальный метод str - возвращает сведения о доме
-        return f'Название: {self.name}, в здании: {self.number_floor} {self.end_floor(self.number_floor)}'
+        return f'Название: {self.name}, в здании: {self.number_floor} {self.end_floor_rp(self.number_floor)}'
+
+    def __del__(self):
+
+        print(f'{self.name} снесён, но он останется в истории')
 
     def go_to (self, new_floor): # Метод выводит каждый этаж до которого нужно приехать включительно, если этаж <1 или больше, чем в доме, выводит строку об отсутствии этажа
         if new_floor <= self.number_floor and new_floor > 0:
             for i in range(1, new_floor+1):
                 print (str(i)+' этаж')
         else:
-            print(f'{new_floor} - такого этажа не существует в "{self.name}", в нем {self.number_floor} {self.end_floor(self.number_floor)}')
+            print(f'{new_floor} - такого этажа не существует в "{self.name}", в нем {self.number_floor} {self.end_floor_rp(self.number_floor)}')
