194e53f85f3ec55d027f7a5192d7a3316bba3d46
fertmi <fertmi@mail.ru>
2024-12-26 14:01:37 +0300

    Задача "История строительства"

--- module_5_4.py
+++ module_5_4.py
@@ -1,16 +1,18 @@
 from class_House import House
 
+def print_history():
+    return 'Дома записанные в историю: ' + ', '.join(House.houses_history)
 s1 = House('ЖК Эльбрус', 10)
-print(House.houses_history)
+print(print_history())
 s2 = House('ЖК Акация', 20)
-print(House.houses_history)
+print(print_history())
 s3 = House('ЖК Матрёшки', 20)
 print(s2==s3)
 print(s1<s2)
 House.go_to(s1,3)
 print(s1==s3)
-print(House.houses_history)
+print(print_history())
 # Удаление объектов
 del s2
 del s3
-print(House.houses_history)
+print(print_history())
